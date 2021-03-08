3c8b1142e3ceabd27cabd30dc168882794fd0719
Robert Richer <robert.richer@fau.de>
2021-03-08 12:23:02 +0100

    added helper function to describe a dataframe by groups

--- carwatch_analysis/carwatch_analysis/general_helper.py
+++ carwatch_analysis/carwatch_analysis/general_helper.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Optional
 
 import pandas as pd
 
@@ -13,3 +13,10 @@ def concat_nights(list_df: Sequence[pd.DataFrame]) -> pd.DataFrame:
     df = pd.concat(list_df, ignore_index=True)
     df.index.name = "night"
     return df
+
+
+def describe_groups_df(data: pd.DataFrame, variable: str, order: Optional[Sequence[str]] = None) -> pd.DataFrame:
+    data = data.unstack().mean(axis=1).groupby(variable).describe()
+    if order:
+        data = data.reindex(order)
+    return data

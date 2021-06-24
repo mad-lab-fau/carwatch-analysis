10b8a918b804f4a8c3e1ba6601dd4a46ef3265fc
Robert Richer <robert.richer@fau.de>
2021-06-24 18:28:57 +0200

    updated saliva analysis helper functions to new biopsykit version

--- carwatch_analysis/carwatch_analysis/saliva_helper.py
+++ carwatch_analysis/carwatch_analysis/saliva_helper.py
@@ -9,9 +9,9 @@ from biopsykit.stats import StatsPipeline
 def import_cortisol_raw(path: path_t) -> pd.DataFrame:
     cort_samples = pd.read_csv(path)
     # assign each night an unique_id to allow repeated measures analyses and insert into dataframe
-    cort_samples.insert(2, 'night_id', cort_samples['subject'] + '_' + cort_samples['night'].astype(str))
-    cort_samples = cort_samples.drop(columns='time')
-    cort_samples = cort_samples.set_index(list(cort_samples.columns.drop('cortisol')))
+    cort_samples.insert(2, "night_id", cort_samples["subject"] + "_" + cort_samples["night"].astype(str))
+    cort_samples = cort_samples.drop(columns="time")
+    cort_samples = cort_samples.set_index(list(cort_samples.columns.drop("cortisol")))
     return cort_samples
 
 
@@ -23,50 +23,32 @@ def import_cortisol_features(path: path_t) -> pd.DataFrame:
 
 def analysis_saliva_raw(data: pd.DataFrame, variable: str) -> StatsPipeline:
     pipeline = StatsPipeline(
-        steps=[
-            ('prep', 'normality'),
-            ('prep', 'equal_var'),
-            ('test', 'mixed_anova'),
-            ('posthoc', 'pairwise_ttests')
-        ],
+        steps=[("prep", "normality"), ("prep", "equal_var"), ("test", "mixed_anova"), ("posthoc", "pairwise_ttests")],
         params={
-            'prep__groupby': 'sample',
-            'prep__group': variable,
-            'dv': 'cortisol',
-            'between': variable,
-            'within': 'sample',
-            'subject': 'night_id',
-            'padjust': 'bonf'
-        }
+            "dv": "cortisol",
+            "between": variable,
+            "within": "sample",
+            "subject": "night_id",
+            "padjust": "bonf",
+        },
     )
 
     pipeline.apply(data)
+
     return pipeline
 
 
-def analysis_saliva_features(data: pd.DataFrame, variable: str,
-                             test_type: Optional[str] = 'welch_anova') -> StatsPipeline:
-    if test_type == 'welch_anova':
-        posthoc = 'pairwise_gameshowell'
+def analysis_saliva_features(
+    data: pd.DataFrame, variable: str, test_type: Optional[str] = "welch_anova"
+) -> StatsPipeline:
+    if test_type == "welch_anova":
+        posthoc = "pairwise_gameshowell"
     else:
-        posthoc = 'pairwise_ttests'
+        posthoc = "pairwise_ttests"
 
-    steps = [
-        ('prep', 'normality'),
-        ('prep', 'equal_var'),
-        ('test', test_type),
-        ('posthoc', posthoc)
-    ]
-    params = {
-        'groupby': 'saliva_feature',
-        'dv': 'cortisol',
-        'between': variable,
-        'padjust': 'bonf'
-    }
-    pipeline = StatsPipeline(
-        steps=steps,
-        params=params
-    )
+    steps = [("prep", "normality"), ("prep", "equal_var"), ("test", test_type), ("posthoc", posthoc)]
+    params = {"groupby": "saliva_feature", "dv": "cortisol", "between": variable, "padjust": "bonf"}
+    pipeline = StatsPipeline(steps=steps, params=params)
 
     pipeline.apply(data)
     return pipeline

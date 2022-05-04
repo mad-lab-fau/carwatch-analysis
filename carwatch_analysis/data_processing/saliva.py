1df15b5f73b07084cfb4dd1eade0db7a18b79b67
Robert Richer <robert.richer@fau.de>
2022-05-04 09:57:03 +0200

    added functions for computing saliva features

---
+++ carwatch_analysis/data_processing/saliva.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+import biopsykit as bp
+
+
+__all__ = ["compute_saliva_features"]
+
+
+def compute_saliva_features(data: pd.DataFrame) -> pd.DataFrame:
+    cort_feat = [
+        bp.saliva.auc(data, saliva_type="cortisol"),
+        bp.saliva.max_increase(data),
+        bp.saliva.slope(data, sample_idx=[0, 3]),
+        bp.saliva.slope(data, sample_idx=[0, 4]),
+        bp.saliva.max_value(data),
+        bp.saliva.initial_value(data),
+    ]
+    cort_feat = pd.concat(cort_feat, axis=1)
+    return bp.saliva.utils.saliva_feature_wide_to_long(cort_feat, saliva_type="cortisol")

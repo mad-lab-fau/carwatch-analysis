4fc99430a425f18d5da4d6519f84ef5fd78098cb
Robert Richer <robert.richer@fau.de>
2022-10-23 10:27:19 +0200

    1.0.0: submission to PNEC

--- carwatch_analysis/__init__.py
+++ carwatch_analysis/__init__.py
@@ -1,5 +1,5 @@
 """A python package to analyze data from the CARWatch Project."""
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __all__ = ["datasets", "exceptions", "plotting", "utils", "data_cleaning", "data_processing", "stats"]

5b0c813c898dd1363e817ac86492527d0bbf0d1c
Robert Richer <robert.richer@fau.de>
2023-01-11 09:07:10 +0100

    moved "auc_increasing" computation from notebook to package

--- carwatch_analysis/data_processing/saliva.py
+++ carwatch_analysis/data_processing/saliva.py
@@ -1,4 +1,5 @@
 import biopsykit as bp
+import numpy as np
 import pandas as pd
 
 __all__ = ["compute_saliva_features"]
@@ -15,3 +16,22 @@ def compute_saliva_features(data: pd.DataFrame) -> pd.DataFrame:
     ]
     cort_feat = pd.concat(cort_feat, axis=1)
     return bp.saliva.utils.saliva_feature_wide_to_long(cort_feat, saliva_type="cortisol")
+
+
+def compute_auc_increasing(data: pd.DataFrame, auc_type: str) -> pd.DataFrame:
+    data = data.xs(auc_type, level="saliva_feature")
+    data = data.unstack("reporting_type")["cortisol"].dropna()
+    data = _columnwise_difference(data).round(2)
+    data_increase = data > 0
+    data_increase = data_increase.apply(lambda df: df.value_counts(normalize=True) * 100)
+    data_increase = data_increase.reindex([True, False]).round(0)
+    data_increase.index.name = "increasing"
+    return data_increase
+
+
+def _columnwise_difference(data: pd.DataFrame) -> pd.DataFrame:
+    cols = data.columns
+    df = data.values
+    r, c = np.triu_indices(df.shape[1], 1)
+    new_cols = [cols[i] + " | " + cols[j] for i, j in zip(r, c)]
+    return pd.DataFrame(df[:, c] - df[:, r], columns=new_cols, index=data.index)
